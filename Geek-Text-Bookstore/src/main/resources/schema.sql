af9da198ef71f3f65dc4f4fffc48d1136bf29bdc
GROUP 1 <57880227+Server101@users.noreply.github.com>
2020-09-14 17:18:06 -0400

    Create schema.sql
    SQL Database Tables

---
+++ Geek-Text-Bookstore/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+
+/*SQL tables for the embedded database*/
+/*Testing Deployment*/
+
+DROP TABLE IF EXISTS TBL_EMPLOYEES;
+  
+CREATE TABLE TBL_EMPLOYEES (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL,
+  email VARCHAR(250) DEFAULT NULL
+);
